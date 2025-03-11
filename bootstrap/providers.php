82179446916611b47d19c9d674f7b086f1cd9a42
Roberto André Bocanegra Palacios <rbocanegra@ctdevelopers.com>
2025-03-11 12:03:29 -0500

    testing wsdl

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,5 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Artisaninweb\SoapWrapper\ServiceProvider::class,
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Artisaninweb\SoapWrapper\SoapWrapper;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('test', function (SoapWrapper $soapWrapper) {
+    $soapWrapper->add('EntidadWS', function ($service) {
+        $service
+            ->wsdl('http://mpv-iotd.gobiernodigital.gob.pe/wsentidad/Entidad?wsdl')
+            ->trace(true);
+    });
+
+    $response = $soapWrapper->call('EntidadWS.getListaEntidad', ['sidcatent' => '1']);
+    dd($response);
 });
